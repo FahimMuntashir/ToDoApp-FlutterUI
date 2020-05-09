8099fb8115d6e8350f6f55f451690df974a994d3
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-10 02:38:41 +0600

    start alignment

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -6,39 +6,50 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: Container(
-        padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30.0,
-              child: Icon(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  radius: 30.0,
+                  child: Icon(
 
-                Icons.list,
+                    Icons.list,
 
-                size: 30.0,
-              ),
+                    size: 30.0,
+                  ),
 
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text('ToDo List ',style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 29.0,
+                ),),
+
+              ],
             ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Text('ToDo List ',style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              fontSize: 29.0,
-            ),),
-            Container(
-              height: 300.0,
+
+          ),
+          Expanded(
+            child: Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(topLeft: Radius.circular(30.0), topRight: Radius.circular(30.0)),
               ),
             ),
-          ],
-        ),
+          ),
+
+
+
+        ],
       ),
     );
   }
