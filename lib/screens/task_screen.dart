36089669ed9c6a83b3ec40cfbbb594ec4dfce83f
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-10 02:12:45 +0600

    add icon

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TaskScreen extends StatelessWidget {
@@ -5,13 +6,30 @@ class TaskScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.lightBlueAccent,
-      body: SafeArea(
+      body: Container(
+        padding: EdgeInsets.only(top: 60.0,left: 30.0,right: 30.0,bottom: 30.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Icon(
-              Icons.list,
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 30.0,
+              child: Icon(
+
+                Icons.list,
+
+                size: 30.0,
+              ),
+
             ),
-            Text('ToDo '),
+            SizedBox(
+              height: 15.0,
+            ),
+            Text('ToDo List ',style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+              fontSize: 29.0,
+            ),),
           ],
         ),
       ),
