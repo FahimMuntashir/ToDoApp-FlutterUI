147ccb8813f29fd6380a3652218f11c910a74c59
FahimMuntashir <fahim.tech.me@gmail.com>
2020-05-12 02:54:29 +0600

    add List and Tile

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -6,23 +6,39 @@ class AddTaskScreen extends StatelessWidget {
     return Container(
       color: Color(0xFF757575),
       child: Container(
+        padding: EdgeInsets.all(30.0),
         decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topLeft: Radius.circular(20.0),
-            topRight: Radius.circular(20.0),
-          )
-        ),
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20.0),
+              topRight: Radius.circular(20.0),
+            )),
         child: Column(
+
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+
           children: <Widget>[
-            Text('Add Task'),
-            TextField(),
-            FlatButton(),
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 28.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Add'),
+              color: Colors.lightBlueAccent,
+            ),
           ],
         ),
       ),
-
-
     );
   }
 }
